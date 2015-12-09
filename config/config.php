28313d3df5703fb4620f1b112da23858789c2e58
matzke <mace@edditrex.de>
2015-12-09 12:27:50 +0100

    add static to combine js

--- config/config.php
+++ config/config.php
@@ -6,9 +6,9 @@
 if(TL_MODE == 'FE')
 {
 	array_insert($GLOBALS['TL_JAVASCRIPT'], 0, array(
-		'haste_plus' => '/system/modules/haste_plus/assets/js/haste_plus.js',
-		'haste_plus_environment' => '/system/modules/haste_plus/assets/js/environment.js',
-		'haste_plus_files' => '/system/modules/haste_plus/assets/js/files.js',
-		'haste_plus_arrays' => '/system/modules/haste_plus/assets/js/arrays.js'
+		'haste_plus' => '/system/modules/haste_plus/assets/js/haste_plus.js|static',
+		'haste_plus_environment' => '/system/modules/haste_plus/assets/js/environment.js|static',
+		'haste_plus_files' => '/system/modules/haste_plus/assets/js/files.js|static',
+		'haste_plus_arrays' => '/system/modules/haste_plus/assets/js/arrays.js|static'
 	));
 }
