d167023259b01977a9261baf1d9bfaa74a66f6c2
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-02-10 10:19:21 +0100

    added dom js class

---
+++ assets/js/dom.js
@@ -0,0 +1,15 @@
+(function($){
+
+	$.extend(HASTE_PLUS, {
+		getTextWithoutChildren: function(element, notrim)
+		{
+			var result = element.clone().children().remove().end().text();
+
+			if (typeof notrim !== 'undefined' && notrim == true)
+				return result;
+			else
+				return result.trim();
+		}
+	});
+
+}(jQuery));
--- config/config.php
+++ config/config.php
@@ -6,9 +6,10 @@
 if(TL_MODE == 'FE')
 {
 	array_insert($GLOBALS['TL_JAVASCRIPT'], 0, array(
-		'haste_plus' => '/system/modules/haste_plus/assets/js/haste_plus.js|static',
-		'haste_plus_environment' => '/system/modules/haste_plus/assets/js/environment.js|static',
-		'haste_plus_files' => '/system/modules/haste_plus/assets/js/files.js|static',
-		'haste_plus_arrays' => '/system/modules/haste_plus/assets/js/arrays.js|static'
+		'haste_plus' => '/system/modules/haste_plus/assets/js/haste_plus.min.js|static',
+		'haste_plus_environment' => '/system/modules/haste_plus/assets/js/environment.min.js|static',
+		'haste_plus_files' => '/system/modules/haste_plus/assets/js/files.min.js|static',
+		'haste_plus_arrays' => '/system/modules/haste_plus/assets/js/arrays.min.js|static',
+		'haste_plus_dom' => '/system/modules/haste_plus/assets/js/dom.min.js|static'
 	));
 }
