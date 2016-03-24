6f3aeb57d85c8730aba47974d2f0a25160be90e9
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2016-03-24 16:25:16 +0100

    cache methods removed

--- library/Haste/Cache/Cache.php
+++ library/Haste/Cache/Cache.php
@@ -6,6 +6,7 @@
  *
  * @author  Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
  * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+ *
  */
 
 namespace HeimrichHannot\Haste\Cache;
@@ -47,63 +48,5 @@ abstract class Cache extends CacheManager
 		);
 		
 		return $arrOptions;
-	}
-
-
-	public function get($keyword)
-	{
-		return self::__callStatic(self::$driver, $keyword, self::getOptions());
-	}
-
-	public function set($keyword, $data, $timeout=null)
-	{
-		if($timeout === null)
-		{
-			$timeout = static::$timeout;
-		}
-
-		$options = self::getOptions();
-
-		return self::__callStatic('driver_set', array($keyword, $data, $timeout, self::getOptions()));
-	}
-
-	public function delete($keyword)
-	{
-		return self::__callStatic('driver_delete', array($keyword, self::getOptions()));
-	}
-
-	public function clean()
-	{
-		return self::__callStatic('driver_clean', array(self::getOptions()));
-	}
-
-	public function touch($keyword, $timeoutextend)
-	{
-		return self::__callStatic('driver_touch', array($keyword, $timeoutextend, self::getOptions()));
-	}
-
-	public function increment($keyword, $step=1)
-	{
-		return self::__callStatic('driver_increment', array($keyword, $step, self::getOptions()));
-	}
-
-	public function decrement($keyword, $step=1)
-	{
-		return self::__callStatic('driver_decrement', array($keyword, $step, self::getOptions()));
-	}
-
-	public function search($needle, $searchInValues = false)
-	{
-		return self::__callStatic('driver_search', array($needle, (boolean) $searchInValues, self::getOptions()));
-	}
-
-	public function isExisting($keyword)
-	{
-		return self::__callStatic('driver_isExisting', array($keyword, self::getOptions()));
-	}
-
-	public function stats()
-	{
-		return self::__callStatic('driver_stats', array(self::getOptions()));
 	}
 }
