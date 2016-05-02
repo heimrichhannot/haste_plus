37ecc8edb0dfe91f6a18de4c301ae58c6d6846dc
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-05-02 16:35:19 +0200

    added getformattedbydca

--- library/Haste/Util/Arrays.php
+++ library/Haste/Util/Arrays.php
@@ -248,4 +248,17 @@ class Arrays
 
 		return false;
 	}
+
+	public static function flattenArray(array $array)
+	{
+		$return = array();
+		array_walk_recursive(
+			$array,
+			function ($a) use (&$return) {
+				$return[] = $a;
+			}
+		);
+
+		return $return;
+	}
 }
