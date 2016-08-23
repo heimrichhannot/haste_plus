5d0af11eefc018317df35b1b5fcb3587bc4f4941
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2016-08-23 10:14:13 +0200

    added Classes::getConstantsByPrefixes()

--- library/Haste/Util/Classes.php
+++ library/Haste/Util/Classes.php
@@ -14,7 +14,8 @@ namespace HeimrichHannot\Haste\Util;
 
 class Classes
 {
-	public static function getParentClasses($strClass, $arrParents=array()) {
+	public static function getParentClasses($strClass, $arrParents = array())
+	{
 		$strParent = get_parent_class($strClass);
 		if ($strParent)
 		{
@@ -22,6 +23,38 @@ class Classes
 
 			$arrParents = self::getParentClasses($strParent, $arrParents);
 		}
+
 		return $arrParents;
+	}
+
+	/**
+	 * Filter class constants by given prefixes and return the extracted constants.
+	 *
+	 * @param  string $strClass            The class that should be searched for constants in.
+	 * @param array   $arrPrefixes         An array of prefixes that should be used to filter the class constants.
+	 * @param         $blnReturnValueAsKey boolean Return the extracted array keys from its value, if true.
+	 *
+	 * @return array The extracted constants as array.
+	 */
+	public static function getConstantsByPrefixes($strClass, array $arrPrefixes = array(), $blnReturnValueAsKey = true)
+	{
+		$arrExtract = array();
+
+		if (!class_exists($strClass))
+		{
+			return $arrExtract;
+		}
+
+		$objReflection = new \ReflectionClass($strClass);
+		$arrConstants  = $objReflection->getConstants();
+
+		if (!is_array($arrConstants))
+		{
+			return $arrExtract;
+		}
+
+		$arrExtract = Arrays::filterByPrefixes($arrConstants, $arrPrefixes);
+
+		return $blnReturnValueAsKey ? array_combine($arrExtract, $arrExtract) : $arrExtract;
 	}
 }
