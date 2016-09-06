0fb1d0640501e337f5f6d4be5fc16faf3c06a9e7
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2016-09-06 14:05:16 +0200

    added findActiveByGroups method

--- library/Haste/Model/UserModel.php
+++ library/Haste/Model/UserModel.php
@@ -14,6 +14,34 @@ namespace HeimrichHannot\Haste\Model;
 class UserModel extends \Contao\UserModel
 {
 	/**
+	 * Find active users by given user groups
+	 *
+	 * @param array $arrGroups
+	 * @param array $arrOptions
+	 *
+	 * @return \UserModel|\UserModel[]|\Model\Collection|null
+	 */
+	public static function findActiveByGroups(array $arrGroups, array $arrOptions = array())
+	{
+		if(empty($arrGroups))
+		{
+			return null;
+		}
+
+		$t    = static::$strTable;
+		$time = \Date::floorToMinute();
+
+		$arrColumns = array("$t.login='1' AND ($t.start='' OR $t.start<='$time') AND ($t.stop='' OR $t.stop>'" . ($time + 60) . "') AND $t.disable=''");
+
+		if (!empty(array_filter($arrGroups)))
+		{
+			$arrColumns[] = QueryHelper::createWhereForSerializedBlob('groups', array_filter($arrGroups));
+		}
+
+		return static::findBy($arrColumns, null, $arrOptions);
+	}
+
+	/**
 	 * Find active user by id
 	 *
 	 * @param int   $intId
