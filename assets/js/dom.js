e83687914170a3342e0a7df3a79ea02157ca2440
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-11-09 14:55:11 +0100

    added scrolling

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -9,6 +9,34 @@
 				return result;
 			else
 				return result.trim();
+		},
+		scrollTo: function($anchor, offsetSize, delay, force) {
+			setTimeout(function () {
+				if (!HASTE_PLUS.elementInViewport($anchor) || typeof force !== 'undefined')
+				{
+					$('html, body').animate({scrollTop: ($anchor.offset().top - offsetSize)}, 'slow');
+				}
+			}, delay);
+		},
+		elementInViewport: function(el) {
+			el = el.get(0);
+			var top = el.offsetTop;
+			var left = el.offsetLeft;
+			var width = el.offsetWidth;
+			var height = el.offsetHeight;
+
+			while(el.offsetParent) {
+				el = el.offsetParent;
+				top += el.offsetTop;
+				left += el.offsetLeft;
+			}
+
+			return (
+				top < (window.pageYOffset + window.innerHeight) &&
+				left < (window.pageXOffset + window.innerWidth) &&
+				(top + height) > window.pageYOffset &&
+				(left + width) > window.pageXOffset
+			);
 		}
 	});
 
