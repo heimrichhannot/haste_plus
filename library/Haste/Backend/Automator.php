a164e30a59e6adfc9a1a9d56d90f83988c64ab5f
Dennis Patzer <patzer.dennis@googlemail.com>
2022-03-23 19:23:56 +0100

    see cl 1.15.4

--- library/Haste/Backend/Automator.php
+++ library/Haste/Backend/Automator.php
@@ -13,28 +13,29 @@ namespace HeimrichHannot\Haste\Backend;
 
 class Automator extends \Automator
 {
-	public function purgePhpFastCache()
-	{
-		if (!is_array($GLOBALS['TL_PURGE']['folders']['phpfastcache']['affected']))
-		{
-			return false;
-		}
+    public function purgePhpFastCache()
+    {
+        if (!is_array($GLOBALS['TL_PURGE']['folders']['phpfastcache']['affected'])) {
+            return false;
+        }
 
-		foreach ($GLOBALS['TL_PURGE']['folders']['phpfastcache']['affected'] as $folder)
-		{
-			// Purge folder
-			$objFolder = new \Folder($folder);
-			$objFolder->purge();
+        foreach ($GLOBALS['TL_PURGE']['folders']['phpfastcache']['affected'] as $folder) {
+            // Purge folder
+            $objFolder = new \Folder($folder);
+            $objFolder->purge();
 
-			// Restore the index.html file
-			$objFile = new \File('templates/index.html', true);
-			$objFile->copyTo($folder . 'index.html');
-		}
+            // Restore the index.html file
+            $objFile = new \File('templates/index.html', true);
 
-		// Also empty the page cache so there are no links to deleted scripts
-		$this->purgePageCache();
+            if ($objFile->exists()) {
+                $objFile->copyTo($folder . 'index.html');
+            }
+        }
 
-		// Add a log entry
-		$this->log('Purged the phpfastcache cache', 'HeimrichHannot\Haste\Backend\Automator purgePhpFastCache()', TL_CRON);
-	}
+        // Also empty the page cache so there are no links to deleted scripts
+        $this->purgePageCache();
+
+        // Add a log entry
+        $this->log('Purged the phpfastcache cache', 'HeimrichHannot\Haste\Backend\Automator purgePhpFastCache()', TL_CRON);
+    }
 }
